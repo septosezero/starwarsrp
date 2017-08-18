9767a26f36b09d9ab8df7d7a98f5754deb8805e5
Anthony Fuller <anthony.fuller.gmod@gmail.com>
2017-08-18 16:23:08 +0100

    Added comments in jobrelated.lua
    Comments on how to do it.

--- gamemodes/starwarsrp/gamemode/config/jobrelated.lua
+++ gamemodes/starwarsrp/gamemode/config/jobrelated.lua
@@ -3,79 +3,55 @@
 -- You should not copy this line into your code.
 local GAMEMODE = GAMEMODE or GM
 
-TEAM_CADET = DarkRP.createJob("Clone Cadet", {
-	color = Color(120, 120, 120, 255),
-	model = {"models/player/testc/cgi cadet.mdl"},
-	description = [[Clone Cadet]],
-	weapons = {"climb_swep2", "salute", "weapon_752_dc15atrainingnodmg"},
-	command = "cadet",
-	max = 0,
-	salary = GAMEMODE.Config.normalsalary,
-	admin = 0,
-	vote = false,
-	hasLicense = false,
-	candemote = false,
-	category = "Cadets",
-	sortOrder = 1,
-	PlayerLoadout = function(ply)
-		ply:SetHealth(100)
-		ply:SetMaxHealth(100)
-	end,
-})
+--	Below is an example of how a job should look
+--TEAM_CADET = DarkRP.createJob("Clone Cadet", {
+--	color = Color(120, 120, 120, 255),
+--	model = {"models/player/testc/cgi cadet.mdl"},
+--	description = [[Clone Cadet]],
+--	weapons = {"climb_swep2", "salute", "weapon_752_dc15atrainingnodmg"},
+--	command = "cadet",
+--	max = 0,
+--	salary = GAMEMODE.Config.normalsalary,
+--	admin = 0,
+--	vote = false,
+--	hasLicense = false,
+--	candemote = false,
+--	category = "Cadets",
+--	sortOrder = 1,
+--	PlayerLoadout = function(ply)
+--		ply:SetHealth(100)
+--		ply:SetMaxHealth(100)
+--	end,
+--})
 
-TEAM_CLONE = DarkRP.createJob("Clone Trooper", {
-	color = Color(120, 120, 120, 255),
-	model = {"models/player/testp2c/cgi ctp2.mdl"},
-	description = [[Clone Trooper]],
-	weapons = { "itemstore_pickup", "climb_swep2", "tfa_dc15a_expanded", "salute", "clone_card_c31"},
-	command = "ct",
-	max = 0,
-	salary = 25,
-	admin = 0,
-	vote = false,
-	hasLicense = false,
-    category = "Clone Trooper",
-    sortOrder = 101,
-	PlayerLoadout = function(ply)
-		ply:SetHealth(100)
-		ply:SetMaxHealth(100)
-	end,
-})
-
--- Compatibility for when default teams are disabled
+-- Compatibility for when default teams are disabled DO NOT CHANGE
 TEAM_CADET = TEAM_CADET  or -1
 
 -- Agendas
-DarkRP.createAgenda("Change this to what job functions as cops", {TEAM_CADET}, {TEAM_CADET})
+-- Example
+--DarkRP.createAgenda("Cadet's Agenda", {TEAM_CADET}, {TEAM_CADET})
+-- First TEAM_ is who can edit separate in the brackets with commas
+--The second TEAM_ is who can view it.
 
 -- Group chats
---DarkRP.createGroupChat(TEAM_CADET)
+--GAMEMODE:AddGroupChat(TEAM_CADET, TEAM_1)
 GAMEMODE:AddGroupChat(TEAM_CADET)
-GAMEMODE:AddGroupChat(TEAM_CLONE)
 
 -- Initial team when first spawning
+--Change this to your default job
 GAMEMODE.DefaultTeam = TEAM_CADET
 
--- Teams that belong to Civil Protection
+-- Teams that belong to Civil Protection they have /wanted /warrant
 GAMEMODE.CivilProtection = {
-    [TEAM_CADET] = true,
 }
 
--- Default categories
-DarkRP.createCategory{
-    name = "Clone Troopers",
-    categorises = "jobs",
-    startExpanded = true,
-    color = Color(0, 107, 0, 255),
-    canSee = fp{fn.Id, true},
-    sortOrder = 100,
-}
-
-DarkRP.createCategory{
-    name = "Cadets",
-    categorises = "jobs",
-    startExpanded = true,
-    color = Color(0, 107, 0, 255),
-    canSee = fp{fn.Id, true},
-    sortOrder = 1,
-}
+-- Categories
+-- How one should look like
+--DarkRP.createCategory{
+--    name = "Cadets",
+--    categorises = "jobs",
+--    startExpanded = true,
+--    color = Color(0, 107, 0, 255),
+--    canSee = fp{fn.Id, true},
+--    sortOrder = 1,
+--}
